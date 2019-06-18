81956162f012ef12338ac9c5e3ec3b1f98486245
stefian <v_ianta@yahoo.com>
2019-06-18 12:31:34 -0400

    added a checkout page

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,30 +9,30 @@ const Order = require("../models/order");
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
-    const page = +req.query.page || 1;
-    let totalItems;
-  
-    Product.find()
-      .countDocuments()
-      .then(numProducts => {
-        totalItems = numProducts;
-        return Product.find()
-          .skip((page - 1) * ITEMS_PER_PAGE)
-          .limit(ITEMS_PER_PAGE);
-      })
-      .then(products => {
-        res.render("shop/product-list", {
-          prods: products,
-          pageTitle: "Products",
-          path: "/products",
-          currentPage: page,
-          hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-          hasPreviousPage: page > 1,
-          nextPage: page + 1,
-          previousPage: page - 1,
-          lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-        });
-      })
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then(products => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "Products",
+        path: "/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+      });
+    })
     .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
@@ -41,15 +41,15 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-    const prodId = req.params.productId;
-    Product.findById(prodId)
-      .then(product => {
-        res.render("shop/product-detail", {
-          product: product,
-          pageTitle: product.title,
-          path: "/products"
-        });
-      })
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(product => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products"
+      });
+    })
     .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
@@ -131,6 +131,30 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .removeFromCart(prodId)
     .then(result => {
       res.redirect("/cart");
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error); // passing the error to the Express Err Handling Middleware
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+    req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+          total += p.quantity * p.productId.price;
+      });
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total
+      });
     })
     .catch(err => {
       const error = new Error(err);
