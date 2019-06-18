f6dd2874ecadc386cd396addf70d7a5b1a52864d
stefian <v_ianta@yahoo.com>
2019-06-17 22:06:35 -0400

    retrieveing chunks of data from db with skip and limit

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,6 +6,8 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -41,7 +43,11 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page;
+
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then(products => {
       res.render("shop/index", {
         prods: products,
@@ -181,18 +187,20 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.text("------------------");
       let totalPrice = 0;
       order.products.forEach(prod => {
-          totalPrice += prod.quantity * prod.product.price;
-        pdfDoc.fontSize(14).text(
-          prod.product.title +
-            " - " +
-            prod.quantity +
-            " x " +
-            "$" +
-            prod.product.price
-        );
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              " - " +
+              prod.quantity +
+              " x " +
+              "$" +
+              prod.product.price
+          );
       });
-      pdfDoc.text('---')
-      pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+      pdfDoc.text("---");
+      pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
 
       pdfDoc.end();
 
