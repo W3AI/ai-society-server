21d40cf7100f373e998621725a2ba416114a0e0e
stefian <v_ianta@yahoo.com>
2019-06-18 10:46:48 -0400

    added code for removing the prod article tag from the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,6 +4,9 @@ const deleteProduct = (btn) => {
     const productId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+    // select closest parent/ancestor element with js .closest()
+    productElement = btn.closest('article');
+
     // to Send or receive to current host if no http://server is specified
     fetch('/admin/product/' + productId, {
         method: 'DELETE',
@@ -11,8 +14,14 @@ const deleteProduct = (btn) => {
             'csrf-token': csrf
         }
     }).then(result => {
-        console.log(result);
-    }).catch(err => {
+        return result.json();
+    })
+    .then(data => {
+        console.log(data);
+        // productElement.remove();    // Modern browsers - remove prod element from DOM
+        productElement.parentNode.removeChild(productElement);  // for browsers + IE  version remove prod element from DOM
+    })
+    .catch(err => {
         console.log(err);
     });
 };
