eeeb4cae128c4ab641168cef1381101120a4b6aa
stefian <v_ianta@yahoo.com>
2019-06-18 08:46:31 -0400

    added productId and csrf vars to delete prod in browser/public admin.js

---
+++ public/js/admin.js
@@ -0,0 +1,6 @@
+// this js code will run on the client / browser
+
+const deleteProduct = (btn) => {
+    const productId = btn.parentNode.querySelector('[name=productId]').value;
+    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+};
